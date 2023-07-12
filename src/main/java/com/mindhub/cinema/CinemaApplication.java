deb48b9ae699eaa11b1d9557a5e78082e82a6a28
Lautaro <85114590+lauta2308@users.noreply.github.com>
2023-07-12 12:22:34 -0300

    command line, application properties set up

--- src/main/java/com/mindhub/cinema/CinemaApplication.java
+++ src/main/java/com/mindhub/cinema/CinemaApplication.java
@@ -1,7 +1,10 @@
 package com.mindhub.cinema;
 
+import com.mindhub.cinema.repositories.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CinemaApplication {
@@ -9,5 +12,26 @@ public class CinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
+
+
+
+	@Bean
+	public CommandLineRunner initData(ActorRepository actorRepository, CinemaRoomRepository cinemaRoomRepository, ClientRepository clientRepository, MovieRepository movieRepository, ProductRepository productRepository, PurchaseItemRepository purchaseItemRepository, PurchaseRepository purchaseRepository, ReviewRepository reviewRepository, SeatRepository seatRepository, ShowRepository showRepository, TicketRepository ticketRepository) {
+		return (args) -> {
+
+
+
+
+
+
+
+
+
+		};
+
+	}
+
+
+
 
 }

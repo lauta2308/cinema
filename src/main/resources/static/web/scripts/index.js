61c2969845ed371e8e50f2b83c9bc164dce4504f
Lautaro <85114590+lauta2308@users.noreply.github.com>
2023-07-24 16:01:24 -0300

    added movies carousel to the front

--- src/main/resources/static/web/scripts/index.js
+++ src/main/resources/static/web/scripts/index.js
@@ -11,6 +11,8 @@ createApp({
             slides: 7,
             clientRole: "",
             clientName: "",
+            carruselOffset: 0, // Offset para el carrusel
+            carruselStep: 1,   // Cantidad de elementos que avanzará el carrusel al hacer click en el botón
             
         }
     },
@@ -18,10 +20,16 @@ createApp({
 
         this.loadMovies();
 
-        if(this.checkLocalAuth){
+        if(this.checkLocalAuth()){
+            console.log(this.checkLocalAuth());
             this.getUserLogged();
+            
         }
+      
 
+
+
+      
 
 
     },
@@ -38,14 +46,21 @@ createApp({
             return localStorage.getItem('cineverseLogin')
         },
 
-        getUserLogged(){
-            axios.get("/api/authenticated_user")
-            .then(response => {
-                this.clientRole = response.data.clientRol,
-                this.clientName = response.data.name
-             } )
+        
+             getUserLogged(){
+                 axios.get("/api/authenticated_user")
+                .then(response => {
+                    this.clientRole = response.data.clientRol,
+                    this.clientName = response.data.name
+                 } )
+                 .then(response => localStorage.setItem('cineverseLogin', false));
+        
+                },
+
+               
+        
+
     
-        },
       
         toggleMenu() {
             this.isMenuOpen = !this.isMenuOpen;
@@ -63,6 +78,28 @@ createApp({
             
         },
 
+        moveCarrusel(step) {
+            const carrusel = this.$refs.carrusel;
+            const carruselWidth = carrusel.offsetWidth;
+            const carruselItemWidth = carrusel.querySelector(".carrusel-item").offsetWidth;
+            const carruselItems = carrusel.childElementCount;
+            const totalWidth = carruselItemWidth * carruselItems + (2 * carruselItems - 1) * 2; // 2rem de separación entre elementos
+            const maxOffset = totalWidth - carruselWidth;
+          
+            // Calcula el nuevo offset al mover el carrusel
+            let newOffset = this.carruselOffset - step * carruselItemWidth;
+          
+            // Verifica si se ha llegado al final o al principio del carrusel
+            if (newOffset < -maxOffset) {
+              // Llegó al final, vuelve a comenzar desde el principio
+              newOffset = 0;
+            } else if (newOffset > 0) {
+              // Está en el principio, no permite mover hacia la izquierda
+              newOffset = 0;
+            }
+          
+            this.carruselOffset = newOffset;
+          },
 
 
     
@@ -70,6 +107,9 @@ createApp({
 
 
     },
+
+
+    
 
 
 
